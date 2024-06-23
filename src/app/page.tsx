097dd8e33d01c3e38676414832064e4992ae8b25
sandibi13 <sandipanb223@gmail.com>
2024-06-23 15:55:47 +0530

    added theme support with next-themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
       <h1>Assistify</h1>
+      <p>(The landing page is still under development)</p>
+      <Link href="/sign-in">Get started</Link>
+      <ModeToggle />
     </div>
   );
 }
